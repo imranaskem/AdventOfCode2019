da0cfa647fd16080862abef32f243e9f1669121b
Imran Askem <imran.askem@gmail.com>
2020-01-13 13:42:47 +0000

    Day 2 part 2

--- day02.py
+++ day02.py
@@ -20,9 +20,31 @@ def process_inputs(inputs):
     return inputs[0]
 
 
-inputs = utils.load_csv_file("inputs/day02.csv", __file__)
+def find_specific_output():
+    inputs = utils.load_csv_file("inputs/day02.csv", __file__)
+    specific_output = 19690720
+    output = 0
+    final_noun = 0
+    final_verb = 0
 
-inputs[1] = 12
-inputs[2] = 2
+    for noun in range(0, 99):
 
-print(process_inputs(inputs))
+        for verb in range(0, 99):
+            instance_inputs = inputs[:]
+            instance_inputs[1] = noun
+            instance_inputs[2] = verb
+
+            output = process_inputs(instance_inputs)
+
+            if output == specific_output:
+                final_verb = verb
+                break
+
+        if output == specific_output:
+            final_noun = noun
+            break
+
+    return (100 * final_noun) + final_verb
+
+
+print(find_specific_output())
